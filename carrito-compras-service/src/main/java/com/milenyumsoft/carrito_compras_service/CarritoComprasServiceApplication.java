42ae67e416b6317cc1f3a2ba408e9a7cb9082b49
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-02-15 18:31:32 -0500

    Integración de los microservicios producto-service , venta-service, carrito-compras-service

---
+++ carrito-compras-service/src/main/java/com/milenyumsoft/carrito_compras_service/CarritoComprasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.milenyumsoft.carrito_compras_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoComprasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritoComprasServiceApplication.class, args);
+	}
+
+}
---
+++ carrito-compras-service/src/test/java/com/milenyumsoft/carrito_compras_service/CarritoComprasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milenyumsoft.carrito_compras_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoComprasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
