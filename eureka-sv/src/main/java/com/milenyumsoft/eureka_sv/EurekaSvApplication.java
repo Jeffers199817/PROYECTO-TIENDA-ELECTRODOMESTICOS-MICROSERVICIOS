59f9a37eea76f734112a470df7a2fd42b1671000
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-02-15 16:49:55 -0500

    Integración de servicio tienda

---
+++ eureka-sv/src/main/java/com/milenyumsoft/eureka_sv/EurekaSvApplication.java
@@ -0,0 +1,13 @@
+package com.milenyumsoft.eureka_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSvApplication.class, args);
+	}
+
+}
