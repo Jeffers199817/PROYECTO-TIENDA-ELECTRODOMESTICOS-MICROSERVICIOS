1c4ae97e11a875a1075e29c0354be6f3f97d5854
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-02-18 13:30:04 -0500

    Integración de metodos en el service

--- carrito-compras-service/src/main/java/com/milenyumsoft/carrito_compras_service/service/ICarritoCompraService.java
+++ carrito-compras-service/src/main/java/com/milenyumsoft/carrito_compras_service/service/ICarritoCompraService.java
@@ -1,6 +1,13 @@
 package com.milenyumsoft.carrito_compras_service.service;
 
+import com.milenyumsoft.carrito_compras_service.modelo.CarritoCompra;
+import com.milenyumsoft.carrito_compras_service.modelo.Producto;
+
 public interface ICarritoCompraService {
 
+public CarritoCompra anadirCarritoCompra(Long idCarritoCompra, Long idProducto);
+
+public CarritoCompra eliminarCarritoCompra(Long idCarritoCompra, Long idProducto);
 
 }
+
