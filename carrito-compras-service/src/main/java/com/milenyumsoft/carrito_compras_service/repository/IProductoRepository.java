58c60caaa6b365ef8fe0486c6e778637fa40225d
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-02-20 21:54:27 -0500

    Final de integración de load balancer

--- carrito-compras-service/src/main/java/com/milenyumsoft/carrito_compras_service/repository/IProductoRepository.java
+++ carrito-compras-service/src/main/java/com/milenyumsoft/carrito_compras_service/repository/IProductoRepository.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Repository
-@FeignClient( name= "producto-service", url= "localhost:8081")
+@FeignClient( name= "producto-service")
 public interface IProductoRepository {
 
 
